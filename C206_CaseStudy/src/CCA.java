3331a8487cfa1a758f5d21fd1d7f4ebcfe8ec0d8
21045050 <21045050@DESKTOP-B30H8QT>
2022-08-01 23:55:13 +0800

    CCA

---
+++ C206_CaseStudy/src/CCA.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+
+public class CCA {
+	
+	private String title;
+	private String description;
+	private int classSize;
+	private String day;
+	private LocalDateTime time;
+	private String venue;
+	private String instructor;
+}
