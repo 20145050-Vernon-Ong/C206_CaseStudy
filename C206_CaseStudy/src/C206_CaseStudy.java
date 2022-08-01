7f012e7068fb4de5664712236d31178af932d87e
21045050 <21045050@10.174.115.62>
2022-08-01 15:05:30 +0800

    Update Menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,13 +3,22 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int option = 0;
+		
+		while (option != 3) {
+			Menu();
+			option = Helper.readInt("Enter option > ");
+		}
+		
 	}
 	
 	private static void Menu() {
-		Helper.line("=", 50);
+		Helper.line(30, "=");
 		System.out.println("CCA Registration System");
-		Helper.line("=", 50);
+		Helper.line(30, "=");
+		System.out.println("1.");
+		System.out.println("2.");
+		System.out.println("3. Quit");
 	}
 
 }
