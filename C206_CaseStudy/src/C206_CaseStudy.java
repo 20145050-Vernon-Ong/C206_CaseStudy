311af30ba472040a653a9bc80e9ba106325d919e
21045050 <21045050@10.174.115.62>
2022-08-02 13:47:37 +0800

    update 3.1

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -9,11 +9,9 @@ public class C206_CaseStudy {
 		int option = 0;
 		
 		while (option != 2) {
-			try {
-				runSavingsProcess(adminList);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			Admin login = getLoginAccount(adminList);
+			if (login != null) {
+			C206_CaseStudy.runSavingsProcess(login);
 			}
 			option = Helper.readInt("Enter password > ");
 		}
@@ -47,58 +45,32 @@ public class C206_CaseStudy {
 			if (memberOption == 1) {
 				
 				//Write code here to perform a deposit transaction.
-				RePublicBank.doDeposit(loginAcct);
+				//C206_CaseStudy
 				
 			} else if (memberOption == 2) {
 				//Write code here to perform a withdrawal transaction.
-				RePublicBank.doWithdraw(loginAcct);
+				//C206_CaseStudy
 				
 			} else if (memberOption == 3) {
 				//Write code here to display user's info.
-				loginAcct.display();			
+				//loginAcct.display();			
 			} else if (memberOption == 4) {
 				System.out.println("Logging out.");
 			}
 		}
 	}
 	
-	private static  getLoginAccount(ArrayList<RePublicPay> accountList) {
-		RePublicPay loginAcct = null;
-		int numTries = 0;
-		//Write code here to cycle through the ArrayList of RePublicPay objects and returns an RePublicPay object if the account number and pin is valid.
-		while (loginAcct == null && numTries < MAX_NUM_TRIES) {
-			int acctNum = Helper.readInt("Account number > ");
-			int pin = Helper.readInt("Enter Pin > ");
-			for(RePublicPay op : accountList) {
-			if (op.login(acctNum, pin) == true) {
-				loginAcct = op;
-				System.out.println("Logged in");
-				break;	
-	
-			   }
-		   }
-			numTries++;
-			for (int i = 0; i < accountList.size(); i++) {
-			   if (numTries == MAX_NUM_TRIES && accountList.get(i).getStatus() == 1) {
-				   accountList.get(i).deactivate();
-				   System.out.println("You have exceeded 3 login attempts!");
-				   System.out.println("This account have been locked. Please contact RePublicPay Administrator.");
-				   break;
-				} else if (accountList.get(i).getStatus() == 0) {
-					loginAcct = null;
-					System.out.println("This account is locked. Please contact RePublicPay Administrator.");
-				}
-				
+	private static Admin getLoginAccount(ArrayList<Admin> adminList) {
+		Admin loginAccount = null;
+		while (loginAccount == null) {
+			String enterName = Helper.readString("Enter Name > ");
+			String enterPass = Helper.readString("Enter password > ");
+			for (Admin a : adminList) {
+				if (a.lo)
 			}
 			
-	        //Continue to prompt the user to enter the account number and pin if there are no matches.
-				
-	        //The message "Incorrect account number or pin" should be displayed if no match is found.
-			if (loginAcct == null && numTries < MAX_NUM_TRIES) {
-				System.out.println("Incorrect account or pin");		
-			}	
+			
 		}
-		return loginAcct;
 	}
 
 }
