86c6b401b7354985cf1201e7e4d43b168d482aa8
21045050 <21045050@10.174.115.62>
2022-08-04 11:24:50 +0800

    Update 2.1

---
+++ C206_CaseStudy/src/ccaCordinator.java
@@ -0,0 +1,29 @@
+
+public class ccaCordinator {
+	
+	private String name;
+	private String CCAassigned;
+	private String password;
+	
+	public ccaCordinator(String name, String cCAassigned, String password) {
+		this.name = name;
+		CCAassigned = cCAassigned;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCCAassigned() {
+		return CCAassigned;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	
+	
+}
