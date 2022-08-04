69e168fc6dc5b4e93a4fe1f1852a9bfec5636a6b
21022695 <21022695@10.174.115.111>
2022-08-04 09:40:54 +0800

    extra copy plus error msg student

---
+++ C206_CaseStudy/src/donttouch/CCA.java
@@ -0,0 +1,54 @@
+package donttouch;
+import java.time.LocalDateTime;
+
+public class CCA {
+	
+	private String title;
+	private String description;
+	private int classSize;
+	private String day;
+	private LocalDateTime time;
+	private String venue;
+	private String instructor;
+	
+	public CCA(String title, String description, int classSize, String day, LocalDateTime time, String venue,
+			String instructor) {
+		this.title = title;
+		this.description = description;
+		this.classSize = classSize;
+		this.day = day;
+		this.time = time;
+		this.venue = venue;
+		this.instructor = instructor;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getClassSize() {
+		return classSize;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public String getInstructor() {
+		return instructor;
+	}
+	
+	
+}
